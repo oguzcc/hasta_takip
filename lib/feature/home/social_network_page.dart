546bf13c4395adada6e239d0ec3ebb6dfd1752b2
Oguz Can <oguzcc@gmail.com>
2024-01-16 14:18:27 +0300

    Add chat page

--- lib/feature/home/social_network_page.dart
+++ lib/feature/home/social_network_page.dart
@@ -10,7 +10,49 @@ class SocialNetworkPage extends StatelessWidget {
         title: const Icon(Icons.people, size: 50),
         toolbarHeight: 100,
       ),
-      body: const Center(child: Text('Sosyal Ag')),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ChatCard(title: 'Merhaba', alingment: Alignment.centerLeft),
+            ChatCard(title: 'Merhaba', alingment: Alignment.centerRight),
+            ChatCard(
+                title: 'Bugun daha iyi misiniz?',
+                alingment: Alignment.centerLeft),
+            ChatCard(
+                title: 'Teşekkürler. Siz nasılsınız?',
+                alingment: Alignment.centerRight),
+            ChatCard(
+                title: 'Ben de iyiyim sağolun.',
+                alingment: Alignment.centerLeft),
+            ChatCard(title: 'Iyiyim sen', alingment: Alignment.centerRight),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatCard extends StatelessWidget {
+  const ChatCard({super.key, required this.alingment, required this.title});
+
+  final String title;
+  final AlignmentGeometry alingment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: alingment,
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+          child: Text(title),
+        ),
+      ),
     );
   }
 }
