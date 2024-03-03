ee9368c5d102fe2172cf2db4a13eeeeb145188c3
Mehmet Durmus <mehmetre@gmail.com>
2024-03-03 17:11:56 +0300

    x

--- lib/feature/home/home_page.dart
+++ lib/feature/home/home_page.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     init();
     super.initState();
-    _databaseReference = FirebaseDatabase.instance.ref().child('videos');
+    _databaseReference = FirebaseDatabase.instance.ref().child('lastVideo');
   }
 
   @override
